9696cd1e691943ed567c7f3726d9eedf0ae12138
Lucas Owino <lucowish35@gmail.com>
2020-05-09 23:44:42 +0300

    created user model and logic for authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 # Initializing application
@@ -11,16 +19,24 @@ def create_app(config_name):
 
      # Creating the app configurations
     app.config.from_object(config_options[config_name])
-    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     # Initializing flask extensions
-    #bootstrap.init_app(app)
+    bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # Registering the auth bluprint
+    from .auth import auth as auth_blueprint
+    try:
+        app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+    except:
+        'error'
 
 
     return app
