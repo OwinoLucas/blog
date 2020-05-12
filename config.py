a5235c4e5e250af38611ee4c1e841bf627945eca
Lucas Owino <lucowish35@gmail.com>
2020-05-12 10:34:44 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config:
     '''
     #_API_KEY = os.environ.get('_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lucas:ballislife@localhost/blog'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
@@ -23,7 +22,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
@@ -33,6 +32,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lucas:ballislife@localhost/blog'
     DEBUG = True
 
 config_options = {
